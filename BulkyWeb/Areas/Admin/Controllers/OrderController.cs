d1c43e5c3cb7638dc6ecab803b6a84637c1b49d3
moroz <moroz@MOROZL>
2025-05-20 18:37:05 +0300

    Revert "addRequestHost"
    This reverts commit 96a7e70e00cc49f3c44b20ff2384318f4c3c7108.

--- BulkyWeb/Areas/Admin/Controllers/OrderController.cs
+++ BulkyWeb/Areas/Admin/Controllers/OrderController.cs
@@ -142,7 +142,7 @@ namespace BeautiqueWeb.Areas.Admin.Controllers
                 .GetAll(u => u.OrderHeaderId == OrderVM.OrderHeader.Id, includeProperties: "Product");
 
             //stripe logic
-            var domain = Request.Scheme + "://" + Request.Host.Value + "/";
+            var domain = "https://localhost:7169/";
             var options = new SessionCreateOptions
             {
                 SuccessUrl = domain + $"admin/order/PaymentConfirmation?orderHeaderId={OrderVM.OrderHeader.Id}",
--- BulkyWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyWeb/Areas/Customer/Controllers/CartController.cs
@@ -124,7 +124,7 @@ namespace BeautiqueWeb.Areas.Customer.Controllers
                 //профіль користовуча і нам потрібно зафіксувати платіж
                 //логіка stripe
 
-                var domain = Request.Scheme + "://" + Request.Host.Value + "/";
+                var domain = "https://localhost:7169/";
                 var options = new SessionCreateOptions
                 {
                     SuccessUrl = domain + $"customer/cart/OrderConfirmation?id={ShoppingCartVM.OrderHeader.Id}",
